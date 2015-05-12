676177ea9bb7de39faa87d11852d6358f5fdead1
Cameron Findlay <cameronfindlay123456789@gmail.com>
2015-05-12 12:20:50 +1000

    Updated anykernel

--- anykernel/tmp/anykernel.sh
+++ anykernel/tmp/anykernel.sh
@@ -43,11 +43,47 @@ sed '/ro.debuggable=/ s/0/1/g' -i default.prop
     fi
 fi
 
+# Modidfications to init.rc
+if [ $finnystweaks -eq 0 ] ; then
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/timer_rate/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/timer_rate/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/timer_slack/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/timer_slack/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/min_sample_time/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/min_sample_time/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/hispeed_freq/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/hispeed_freq/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/target_loads/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/target_loads/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/go_hispeed_load/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/go_hispeed_load/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/above_hispeed_delay/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/above_hispeed_delay/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/boost/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/boost/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/boostpulse/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/input_boost/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/input_boost/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/boostpulse_duration/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/boostpulse_duration/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/io_is_busy/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpufreq\/interactive\/io_is_busy/d' -i init.rc
+sed '/# Assume SMP uses shared cpufreq policy for all CPUs/d' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/d' -i init.rc
+fi
+
 # Modifications to init.mako.rc
 if [ $finnystweaks -eq 0 ] ; then
 sed '/group radio system/a \    disabled' -i init.mako.rc
 #sed '/group root system/a \    disabled' -i init.mako.rc
+sed '/scaling_min_freq/ s/384000/192000/g' -i init.mako.rc
 sed '/sys\/class\/timed_output\/vibrator\/amp/ s/70/60/g' -i init.mako.rc
+sed "/cpu3\/cpufreq\/scaling_min_freq/ a\\
+    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1512000\\
+    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1512000\\
+    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1512000\\
+    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1512000" -i init.mako.rc
 fi
 
 # Repack ramdisk
